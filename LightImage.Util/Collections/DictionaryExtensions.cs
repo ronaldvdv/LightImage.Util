8013c4e322d0e1ffc10df4d772f854997c3adb49
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-05-01 12:36:40 +0200

    Adding collections

---
+++ LightImage.Util/Collections/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        // https://stackoverflow.com/questions/469202/best-way-to-remove-multiple-items-matching-a-predicate-from-a-c-sharp-dictionary
+        public static void RemoveAll<TKey, TValue>(this IDictionary<TKey, TValue> dict, Func<TKey, TValue, bool> predicate)
+        {
+            var items = dict.Where(k => predicate(k.Key, k.Value)).ToList();
+            foreach (var item in items)
+                dict.Remove(item.Key);
+        }
+    }
+}
